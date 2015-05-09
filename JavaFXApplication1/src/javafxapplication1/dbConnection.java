9019a9b348d6e8051ee7e6af8388e2cd2b60bcf4
kzabinski <krzysztof.kamil.zabinski@gmail.com>
2015-05-09 20:26:55 +0200

    sesja 09.05.2015

--- JavaFXApplication1/src/javafxapplication1/dbConnection.java
+++ JavaFXApplication1/src/javafxapplication1/dbConnection.java
@@ -30,16 +30,19 @@ public class dbConnection {
 
 //        for (int i = 0; i < load.result.length; i++) {
 //            Statement stmt = con.createStatement();
-//            String update = "insert into test(pierwsza, druga) values('" + load.result[i][0] + "', '" + load.result[i][1] + "')";
+//            String update = "insert into odczytyTestowe values('2015-07-01 09:12:00', 'SERIAL2', 231, 233, 222, 223, 1, 12, 13, 14, 50)";
 //            int lm = stmt.executeUpdate(update);
 //        }
         Statement stmt = con.createStatement();
-        ResultSet lm = stmt.executeQuery("SELECT pierwsza, druga FROM test");
+        ResultSet lm = stmt.executeQuery("SELECT date FROM odczytyTestowe");
         while (lm.next()) {
-
-            pierwsza.add(lm.getString("pierwsza"));
-            druga.add(lm.getString("druga"));
-
+            System.out.println(lm.getString("date"));
         }
+//        while (lm.next()) {
+//
+//            pierwsza.add(lm.getString("pierwsza"));
+//            druga.add(lm.getString("druga"));
+//(date, serial, systemVoltage, L-NVoltagePhase1, L-NVoltagePhase2, L-NVoltagePhase3, systemCurrent, phase1current, phase2current, phase3current, frequency)
+//        }
     }
 }
