6d89815a4424907c2d15537fa675ed1eb35210d6
kzabinski <krzysztof.kamil.zabinski@gmail.com>
2015-05-27 19:43:18 +0200

    Poprawiony program do generacji danych

--- Generate_Test_Data/src/generate_test_data/Generate_Test_Data.java
+++ Generate_Test_Data/src/generate_test_data/Generate_Test_Data.java
@@ -23,12 +23,11 @@ public class Generate_Test_Data {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
 //        int m;
-        LocalDateTime dt_temp;
+//        LocalDateTime dt_temp;
         dbConnection connect = new dbConnection();
         int[] values;
-        values = new int[11];
         int val = 230;
         Random generator = new Random();
         try {
@@ -37,37 +36,42 @@ public class Generate_Test_Data {
         } catch (SQLException | ClassNotFoundException | IOException ex) {
             Logger.getLogger("mylogger").log(Level.SEVERE, null, ex);
         }
-            LocalDateTime dt = LocalDateTime.of(2015, 1, 1, 8, 0);
+        LocalDateTime dt = LocalDateTime.of(2015, 1, 1, 8, 0);
 //            connect.insertDatabase(null, null, );
-            for(int m=0; dt.plusMinutes(m).isBefore(LocalDateTime.of(2015, 3, 31, 0, 0)); m=m+1){
-                dt_temp = dt.plusMinutes(m);
+        for (int m = 0; dt.plusMinutes(m).isBefore(LocalDateTime.of(2015, 3, 31, 0, 0)); m = m + 1) {
+//            dt_temp = dt.plusMinutes(m);
 //                System.out.println(dt_temp);
 //                val = 230 + generator.nextInt()*10-5;
-                values[0] = 250 + generator.nextInt()*10-5;
-                values[1] = 100 + generator.nextInt()*10-5;
-                values[2] = 200 + generator.nextInt()*10-5;
-                values[3] = 300 + generator.nextInt()*10-5;
-                values[4] = 120 + generator.nextInt()*10-5;
-                values[5] = 230 + generator.nextInt()*10-5;
-                values[6] = 310 + generator.nextInt()*10-5;
-                values[7] = 5 + generator.nextInt()*10-5;
-                values[8] = 10 + generator.nextInt()*10-5;
-                values[9] = 20 + generator.nextInt()*10-5;
-                values[10] = 30 + generator.nextInt()*10-5;
-                
-              try {   
-                connect.insertDatabase(dt_temp.toString(),
+            values = new int[11];
+            
+            values[0] = 250 + generator.nextInt() * 10 - 5;
+            values[1] = 100 + generator.nextInt() * 10 - 5;
+            values[2] = 200 + generator.nextInt() * 10 - 5;
+            values[3] = 300 + generator.nextInt() * 10 - 5;
+            values[4] = 120 + generator.nextInt() * 10 - 5;
+            values[5] = 230 + generator.nextInt() * 10 - 5;
+            values[6] = 310 + generator.nextInt() * 10 - 5;
+            values[7] = 5 + generator.nextInt() * 10 - 5;
+            values[8] = 10 + generator.nextInt() * 10 - 5;
+            values[9] = 20 + generator.nextInt() * 10 - 5;
+            values[10] = 30 + generator.nextInt() * 10 - 5;
+
+            values = null;
+            
+            try {
+                connect.insertDatabase(dt.plusMinutes(m).toString(),
                         "SERIAL1",
                         values);
+
             } catch (SQLException | ClassNotFoundException | IOException ex) {
                 Logger.getLogger("mylogger").log(Level.SEVERE, null, ex);
             }
-                if (m%1000 == 0) {
-                    System.out.println(dt_temp);
-                }
+            if (m % 1000 == 0) {
+                System.out.println(dt.plusMinutes(m));
             }
-        
+        }
+
 //        dt+4;
     }
-    
+
 }
